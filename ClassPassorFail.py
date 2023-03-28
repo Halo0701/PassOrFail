ee41987a555c86dd2961fe3849443566d04d6bbc
Halo0701 <127592372+Halo0701@users.noreply.github.com>
2023-03-28 14:22:34 +0300

    Create ClassPassorFail.py

---
+++ ClassPassorFail.py
@@ -0,0 +1,7 @@
+# vize1 = float(input('birinci vize notu: '))
+# vize2 = float(input('ikinci vize notu: '))
+# final = float(input('final notu: '))
+
+# ortalama = (vize1+vize2)*0.3 + final*0.4
+
+# print(f'dersten geçme durumunuz: {ortalama>=50}')
